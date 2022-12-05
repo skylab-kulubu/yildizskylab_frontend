2f3636e6ad0aac67dec713d5b0855eb9a0abde3e
melihyelman <myelman17@gmail.com>
2022-12-05 22:37:21 +0300

    add members page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import Lottie from "lottie-react";
-import animationData from './lotties/404';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [clubs, setClubs] = useState([
+    { id: 1, image: "skylab.jpg", name: "weblab1", members: [{ id: 100, role: "leader", name: "melih yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 101, role: "member", name: "melih yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 102, role: "member", name: "ahmet deniz", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 2, image: "skylab.jpg", name: "weblab2", members: [{ id: 103, role: "leader", name: "ahmet ahmet", image: "https://thispersondoesnotexist.com/image" }, { id: 104, role: "member", name: "meehmet mehmeht", image: "https://thispersondoesnotexist.com/image" }, { id: 101, role: "member", name: "cem cem", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 3, image: "skylab.jpg", name: "weblab3", members: [{ id: 104, role: "leader", name: "abc abc", image: "https://thispersondoesnotexist.com/image" }, { id: 105, role: "member", name: "melih yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 106, role: "member", name: "ahmet deniz", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 4, image: "skylab.jpg", name: "weblab4", members: [{ id: 109, role: "leader", name: "melih yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 108, role: "member", name: "ayşe ayşe", image: "https://thispersondoesnotexist.com/image" }, { id: 107, role: "member", name: "csa wqe", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 5, image: "skylab.jpg", name: "weblab5", members: [{ id: 110, role: "leader", name: "fatma yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 111, role: "member", name: "irem irem", image: "https://thispersondoesnotexist.com/image" }, { id: 121, role: "member", name: "ahmet deniz", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 6, image: "skylab.jpg", name: "weblab6", members: [{ id: 120, role: "leader", name: "egemen egemen", image: "https://thispersondoesnotexist.com/image" }, { id: 131, role: "member", name: "hayriye hayriye", image: "https://thispersondoesnotexist.com/image" }, { id: 141, role: "member", name: "ahmet deniz", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 7, image: "skylab.jpg", name: "weblab7", members: [{ id: 140, role: "leader", name: "melih irem", image: "https://thispersondoesnotexist.com/image" }, { id: 201, role: "member", name: "melih abc", image: "https://thispersondoesnotexist.com/image" }, { id: 181, role: "member", name: "mehmet deniz", image: "https://thispersondoesnotexist.com/image" }] },
+    { id: 8, image: "skylab.jpg", name: "weblab8", members: [{ id: 150, role: "leader", name: "irem yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 301, role: "member", name: "abc yelman", image: "https://thispersondoesnotexist.com/image" }, { id: 191, role: "member", name: "ahmet can", image: "https://thispersondoesnotexist.com/image" }] }
+  ])
+  const [activeClubId, setActiveClubId] = useState(1)
+
+  const clubInfo = clubs.find(club => club.id === activeClubId)
+
   return (
-    <div className='w-full h-full bg-[#0c0a33]'>
-      <div className="h-16 bg-white w-full text-center"> burası header alanı</div>
-      <div className="container mx-auto px-4 flex gap-4 pt-8 flex-col md:flex-row items-center justify-center h-[calc(100%-4rem)] ">
-        <div className='text-center'>
-          <h2 className='text-[#3d99e4] text-4xl md:text-6xl tracking-wider font-extrabold leading-normal mb-8'>Yolunu mu kaybettin?</h2>
-          <span className='text-gray-400 text-md md:text-lg'>Üzgünüm, aradığın sayfayı bulamadım. Anasayfaya dönerek sitemizi inceleyebilirsin.</span>
-        </div>
-        <Lottie
-          animationData={animationData}
-          loop={true}
-          style={{width: "100%", height: "100%", maxHeight: "30rem"}}
-        />
+    <div >
+      <div className='h-12 bg-black text-white text-center'>header</div>
+
+      <div className='bg-[#D9D9D9] my-4 gap-6 py-4 px-6 rounded-md container mx-auto flex justify-center items-center flex-wrap'>
+        {clubs.map(club => (
+          <div onClick={() => setActiveClubId(club.id)} key={club.id} className={`w-16 h-16 ${club.id !== activeClubId ? "opacity-50" : "opacity-100"} hover:opacity-100 border border-gray-500 cursor-pointer rounded-md p-px flex items-center justify-center`}>
+            <img className='w-12 h-12 rounded-full' src={club.image} alt='skylab' />
+          </div>
+        ))}
       </div>
+
+      <div className='container mx-auto flex justify-center items-center my-4 flex-col gap-4 md:gap-8'>
+
+        <h2 className='font-bold text-3xl py-2'>{clubInfo.name}</h2>
+
+        <div className='w-full md:w-[calc(33%-4rem)] h-64 cursor-pointer group perspective '>
+              <div className='relative preserve-3d group-hover:rotate-y-180 w-full h-full duration-700'>
+                <div className='backface-hidden absolute h-full w-full flex flex-col gap-4 justify-center items-center border px-10 py-2 rounded-md shadow-sm'>
+                  <img className='w-24 h-24 rounded-full' src={clubInfo.members[0].image} alt='person' />
+                  <h3 className='font-bold text-2xl'>{clubInfo.members[0].role}</h3>
+                  <p>{clubInfo.members[0].name}</p>
+                </div>
+
+                <div className='absolute rotate-y-180 backface-hidden w-full h-full flex-col gap-4 justify-center items-center border px-10 py-2 rounded-md shadow-sm'>
+                  asdgas
+                </div>
+              </div>
+            </div>
+
+        <div className='w-full h-px bg-slate-200 my-2'></div>
+
+        <div className='w-full flex flex-wrap flex-col md:flex-row items-center justify-center gap-4 pb-4'>
+
+          {clubInfo.members.slice(1).map(member => (
+            <div key={member.id} className='w-full md:w-[calc(33%-4rem)] h-64 cursor-pointer group perspective'>
+              <div className='relative preserve-3d group-hover:rotate-y-180 w-full h-full duration-700'>
+                <div className='backface-hidden absolute h-full w-full flex flex-col gap-4 justify-center items-center border px-10 py-2 rounded-md shadow-sm'>
+                  <img className='w-24 h-24 rounded-full' src={member.image} alt='person' />
+                  <h3 className='font-bold text-2xl'>{member.role}</h3>
+                  <p>{member.name}</p>
+                </div>
+
+                <div className='absolute rotate-y-180 backface-hidden w-full h-full flex-col gap-4 justify-center items-center border px-10 py-2 rounded-md shadow-sm'>
+                  asdgas
+                </div>
+              </div>
+            </div>
+          ))}
+
+        </div>
+      </div>
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,21 @@
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -5,5 +23,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [Myclass],
 }
